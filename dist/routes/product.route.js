afa83e9fee1e9f9c1598a410e1ca5b450392a37c
rania333 <raniaaramadan1999@gmail.com>
2022-11-12 01:26:31 +0200

    add middleware

--- dist/routes/product.route.js
+++ dist/routes/product.route.js
@@ -4,9 +4,10 @@ exports.productRoute = void 0;
 var express_1 = require("express");
 var product_validation_1 = require("../validations/product.validation");
 var product_controller_1 = require("../controllers/product.controller");
+var authenticatedUser_1 = require("../middleware/authenticatedUser");
 exports.productRoute = (0, express_1.Router)();
 exports.productRoute.get('/:id', product_validation_1.getOneProductValidation, product_controller_1.getOneProductController);
 exports.productRoute.route('/')
-    .get(product_controller_1.getAllProductsController)
+    .get(authenticatedUser_1.authenticatedUser, product_controller_1.getAllProductsController)
     .put(product_validation_1.updateProductValidation, product_controller_1.updateProductController)
     .post(product_validation_1.addProductValidation, product_controller_1.addProductController)["delete"](product_validation_1.deleteProductValidation, product_controller_1.deleteProductController);
--- src/routes/product.route.ts
+++ src/routes/product.route.ts
@@ -1,12 +1,13 @@
 import {Router} from 'express'
 import { addProductValidation, deleteProductValidation, getOneProductValidation, updateProductValidation } from '../validations/product.validation';
 import { addProductController, deleteProductController, getAllProductsController, getOneProductController, updateProductController } from '../controllers/product.controller';
+import { authenticatedUser } from '../middleware/authenticatedUser';
 export const productRoute = Router()
 
 productRoute.get('/:id', getOneProductValidation, getOneProductController);
 
 productRoute.route('/')
-.get(getAllProductsController)
+.get(authenticatedUser, getAllProductsController)
 .put(updateProductValidation, updateProductController)
 .post(addProductValidation, addProductController)
 .delete(deleteProductValidation, deleteProductController)
