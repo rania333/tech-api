17d466c24167e46b395c59bd4894c7063c9c5b67
rania333 <raniaaramadan1999@gmail.com>
2022-11-12 04:44:37 +0200

    migrate table

--- migrations/sqls/20221107173026-orders-table-up.sql
+++ migrations/sqls/20221107173026-orders-table-up.sql
@@ -1,5 +1,7 @@
 CREATE TABLE orders (
     id SERIAL PRIMARY KEY,
-    status VARCHAR(50) DEFAULT 'ORDERED',
+    status VARCHAR(50) DEFAULT 'active',
+    quantity INTEGER DEFAULT 1,
+    prodId bigint REFERENCES products (id),
     userId bigint REFERENCES users (id)
 );
--- migrations/sqls/20221107173506-order-products-table-up.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE orderProducts (
-    id SERIAL PRIMARY KEY,
-    quantity INTEGER NOT NULL,
-    productId bigint REFERENCES products(id),
-    orderId bigint REFERENCES orders(id)
-);
